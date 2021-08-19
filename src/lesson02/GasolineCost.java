713ed1785aff3e1eaf3c8189aed487e47e046fae
imagineapril <julia.timokhina604@gmail.com>
2021-08-19 20:55:46 +0300

    GasolineCost

---
+++ src/lesson02/GasolineCost.java
@@ -0,0 +1,16 @@
+/*
+ * GasolineCost
+ *
+ * @version 1.1
+ *
+ * @author imagineapril
+ */
+package lesson02;
+
+public class GasolineCost {
+    public static void main(String[] args) {
+        int liter = 20;
+        int cost = 50;
+        System.out.println("Стоимость бензина составит " + liter * cost);
+    }
+}
