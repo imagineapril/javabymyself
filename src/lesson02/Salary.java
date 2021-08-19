54fa82b216a7278f7313ac70c9c5a8d4fae08498
imagineapril <julia.timokhina604@gmail.com>
2021-08-19 22:35:49 +0300

    Salary

---
+++ src/lesson02/Salary.java
@@ -0,0 +1,9 @@
+package lesson02;
+
+public class Salary {
+    public static void main(String[] args) {
+        int salaryBefore = 50000;
+        int salaryAfter = 50000 - 50000 / 100 * 13;
+        System.out.println("Зарплата на руки составит " + salaryAfter);
+    }
+}
