4b36b91b1189dcbe144c858d8d92481055054bd1
imagineapril <julia.timokhina604@gmail.com>
2021-08-19 19:48:58 +0300

    HelloWorld

---
+++ src/lesson02/HelloWorld.java
@@ -0,0 +1,10 @@
+package lesson02;
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        Date d = new Date();
+        System.out.println("Date:" + d.toString());
+    }
+}
