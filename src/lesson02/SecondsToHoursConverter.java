93bd7f7c62766a283d36ce3c078e9c2380190af2
imagineapril <julia.timokhina604@gmail.com>
2021-08-19 22:46:07 +0300

    SecondsToHoursConverter

---
+++ src/lesson02/SecondsToHoursConverter.java
@@ -0,0 +1,9 @@
+package lesson02;
+
+public class SecondsToHoursConverter {
+    public static void main(String[] args) {
+        int seconds = 7200;
+        int hours = 7200 / 3600;
+        System.out.println(seconds + " секунд составляет " + hours + " часа.");
+    }
+}
